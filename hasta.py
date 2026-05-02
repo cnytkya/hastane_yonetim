65bd1aa81ba59812629edc401b15e2b24862626b
Cuneyt Kayac <kayacuneyt2542@gmail.com>
2026-05-02 14:47:01 +0300

    Refactor hasta_ekle function to improve input handling and validation

--- hasta.py
+++ hasta.py
@@ -1,4 +1,5 @@
 import database # Veritabanına hasta verilerini yazmak, sorgulamak ve silmek için kullanılan modül.
+import utils
 
 def hasta_ekle():
     # yeni hasta kaydeder.
@@ -7,10 +8,15 @@ def hasta_ekle():
     print("\n" + "="*40)
 
     ad = input("Ad: ").strip() # kullanıcıdan adını alır ve başındaki/sonundaki boşlukları temizler
-    ad = input("Soyad: ").strip()
+    soyad = input("Soyad: ").strip()
 
     # tc no kontrolü: amaç kullanıcıdan geçerli bir tc girmesini sağlamak
     while True:
         tc_no = input("T.C No(11 Hane Olacak Şekilde): ").strip()
-        
+        if utils.tc_gecerli_mi(tc_no):
+            break
+        print("Geçersiz TC NO! Lütfen 11 haneli geçerli bir TC giriniz.")
+    
+    telefon = 
+
         
